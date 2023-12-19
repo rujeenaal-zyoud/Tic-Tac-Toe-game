3b1e32b77ce0baab0fd3e4dae3ffc9f977ff6e0d
rujeenaal-zyoud <46718110+rujeenaal-zyoud@users.noreply.github.com>
2023-12-19 13:54:09 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,21 @@
+//import the pakage that need in this game 
+import 'package:flutter/material.dart';
+import 'screens/game.dart';
 
+void main() {
+  //run the Tic Tac Toe
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
+    );
+  }
+}
